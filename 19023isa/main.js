c705266fb04279c5925c3050cb46b2cacd3f4c73
isabellacianorte <127401546+isabellacianorte@users.noreply.github.com>
2023-10-19 08:59:21 -0300

    Add files via upload

---
+++ 19023isa/main.js
@@ -0,0 +1,10 @@
+//pom----------------------------------
+ function tocasompom (){
+    document.querySelector('#som_tecla_pom').play ();
+ }
+
+document.querySelector('.teclka') 
+
+ const listadeteclas
+
+
